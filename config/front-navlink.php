8667877ad5806c38c46be43618ee13e482f1c8dd
David <kontakt@david-grass-feria.de>
2024-05-04 00:49:49 +0200

    Update front-navlink.php

--- config/front-navlink.php
+++ config/front-navlink.php
@@ -3,6 +3,7 @@
 return [
     [
         'title' => env('WIKI_MENU_NAME','Wiki'),
+        'menu_name' => env('WIKI_MENU_NAME','Wiki'),
         'route' => 'front.wiki.index',
         'active' => ['front.wiki.index','front.wiki.show'],
         'order' => env('WIKI_ORDER_FOR_NAV',4),
